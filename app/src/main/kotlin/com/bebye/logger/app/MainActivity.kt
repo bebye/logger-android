38a8e36fc469783d1b35c841f732c2c80eb41558
mkwon <developer.mkwon@gmail.com>
2020-12-08 13:59:51 +0900

    clean up code

--- app/src/main/kotlin/com/bebye/logger/app/MainActivity.kt
+++ app/src/main/kotlin/com/bebye/logger/app/MainActivity.kt
@@ -3,7 +3,6 @@ package com.bebye.logger.app
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bebye.logger.log.Logger
-import com.bebye.logger.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
