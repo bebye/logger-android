74bc8f2c79b271ea641db8538ceffd359d336973
mkwon <developer.mkwon@gmail.com>
2020-12-08 18:39:43 +0900

    add logger

--- app/src/main/kotlin/com/bebye/logger/app/MainActivity.kt
+++ app/src/main/kotlin/com/bebye/logger/app/MainActivity.kt
@@ -9,7 +9,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Logger.v("This is Logger.")
         Logger.d("This is Logger.")
+        Logger.i("This is Logger.")
+        Logger.w("This is Logger.")
+        Logger.e("This is Logger.")
+        Logger.a("This is Logger.")
     }
 
 }
